cb1197185297318b7e36ae8bc0f61800f18dee11
valdemir92 <valdemirpraxedes92@gmail.com>
2017-06-24 14:55:27 -0300

    adicionado o padrão observer

---
+++ Padrão Observer/Alarme/src/com/jun/observer/Observer.java
@@ -0,0 +1,6 @@
+package com.jun.observer;
+
+public interface Observer {
+
+	public void update(int hora, int minuto, int segundo);
+}
---
+++ Padrão Observer/Alarme/src/com/jun/observer/Subject.java
@@ -0,0 +1,10 @@
+package com.jun.observer;
+
+public interface Subject {
+	
+	public void registerObserver(Observer o);
+	
+	public void removeObserver(Observer o);
+	
+	public void notifyObservers();
+}
