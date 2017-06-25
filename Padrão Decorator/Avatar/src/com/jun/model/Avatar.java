4f4f8d3e5d2e415d123f62a0eaa39b7358d460e1
valdemir92 <valdemirpraxedes92@gmail.com>
2017-06-25 11:59:14 -0300

    Criado classe Avatar

---
+++ Padrão Decorator/Avatar/src/com/jun/model/Avatar.java
@@ -0,0 +1,45 @@
+package com.jun.model;
+
+public abstract class Avatar {
+	
+	private String Nome;
+	private int Idade;
+	private GENERO Genero;
+	public static enum GENERO {Masculino, Feminino}
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return Nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	/**
+	 * @return the idade
+	 */
+	public int getIdade() {
+		return Idade;
+	}
+	/**
+	 * @param idade the idade to set
+	 */
+	public void setIdade(int idade) {
+		Idade = idade;
+	}
+	/**
+	 * @return the genero
+	 */
+	public GENERO getGenero() {
+		return Genero;
+	}
+	/**
+	 * @param genero the genero to set
+	 */
+	public void setGenero(GENERO genero) {
+		Genero = genero;
+	}; 
+}
